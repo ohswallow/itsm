028190d03aefe6c52263207175f725588ce21e6e
JeonJongHo <drctork@gmail.com>
2026-05-08 14:21:53 +0900

    [fix] 메인 메뉴의 선택된 메뉴의 표출 문제점 해결 1. <.sidebar_menu_link> function conponents 개발 2. PathProvider에서 current_path에 path + querystring 추가 3. **나의 Crew** (/crews) , **모든 Crews** (/crews/all) 인경우 querystring인 menu_id= 구분한게 개발 ''' url : /crews/all?menu_id=all_crews
    <.sidebar_menu_link menu_id="all_crews" />
    '''
    이슈 번호 : #134

--- lib/itsm_web/hooks/path_provider.ex
+++ lib/itsm_web/hooks/path_provider.ex
@@ -5,7 +5,10 @@ defmodule ItsmWeb.PathProvider do
   def on_mount(:set_current_path_on_assigns, _params, _session, socket) do
     {:cont,
      attach_hook(socket, :set_current_path_on_assigns, :handle_params, fn _params, uri, socket ->
-       {:cont, assign(socket, :current_path, URI.parse(uri).path)}
+       uri = URI.parse(uri)
+       current_path = if uri.query, do: "#{uri.path}?#{uri.query}", else: uri.path
+
+       {:cont, assign(socket, :current_path, current_path)}
      end)}
   end
 end
